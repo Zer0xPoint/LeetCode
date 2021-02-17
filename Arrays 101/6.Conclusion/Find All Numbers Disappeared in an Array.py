34ff658bc0a0c67454d1c490c8f0a5b30aa617b3
Zer0xPoint <liyizhou@me.com>
2021-02-17 19:17:02 +0800

    Array 101 Fin

--- Arrays 101/6.Conclusion/Find All Numbers Disappeared in an Array.py
+++ Arrays 101/6.Conclusion/Find All Numbers Disappeared in an Array.py
@@ -1,13 +1,22 @@
 from typing import List
 
+from pip._vendor.msgpack.fallback import xrange
+
 
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        full_nums_set = set(i for i in range(1, len(nums) + 1))
-        for i in set(nums):
-            if i in nums:
-                full_nums_set.remove(i)
-        return list(full_nums_set)
+        # full_nums_set = set(i for i in range(1, len(nums) + 1))
+        # for i in set(nums):
+        #     if i in nums:
+        #         full_nums_set.remove(i)
+        #
+        # return list(full_nums_set)
+
+        for i in xrange(len(nums)):
+            index = abs(nums[i]) - 1
+            nums[index] = - abs(nums[index])
+
+        return [i + 1 for i in range(len(nums)) if nums[i] > 0]
 
 
 if __name__ == '__main__':
---
+++ Arrays 101/6.Conclusion/Squares of a Sorted Array.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        result_list = []
+        # for i in range(len(nums), 0):
+        i = 0
+        j = len(nums) - 1
+        while i <= j:
+            pre_squ = nums[i] ** 2
+            last_squ = nums[j] ** 2
+            if pre_squ < last_squ:
+                result_list.insert(0, last_squ)
+                j -= 1
+            else:
+                result_list.insert(0, pre_squ)
+                i += 1
+        return result_list
