f98724439f6bc134bceeae9d047dcea3ee27b664
Zer0xPoint <liyizhou@me.com>
2021-02-21 11:44:44 +0800

    add Solution

---
+++ Linked List 101/2.Two Pointer Tecnique/Linked List Circle II.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        slow = head
+        fast = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next.next
+
+
+
+def list_to_linked_list(array_list):
+    head_node = ListNode(array_list[0])
+    node = head_node
+    for i in range(1, len(array_list)):
+        node.next = ListNode(array_list[i])
+        node = node.next
+    return head_node
+
+
+def traverse_linked_list(linked_list_head: ListNode):
+    while linked_list_head:
+        print(linked_list_head.val)
+        linked_list_head = linked_list_head.next
+
+
+if __name__ == '__main__':
+    nums = [3, 2, 0, -4]
+    head = list_to_linked_list(nums)
+    traverse_linked_list(head)
+
--- Linked List 101/2.Two Pointer Tecnique/Linked List Circle.py
+++ Linked List 101/2.Two Pointer Tecnique/Linked List Circle.py
@@ -28,3 +28,40 @@ class Solution:
             return True
         except:
             return False
+
+
+def list_to_linked_list(array_list):
+    head_node = ListNode(array_list[0])
+    node = head_node
+    for i in range(1, len(array_list)):
+        node.next = ListNode(array_list[i])
+        node = node.next
+    return head_node
+
+
+def traverse_linked_list(linked_list_head: ListNode):
+    while linked_list_head:
+        print(linked_list_head.val)
+        linked_list_head = linked_list_head.next
+
+
+def create_circle_in_linked_list(linked_list_head: ListNode, index: int):
+    # traverse to tail
+    tail_node = linked_list_head
+    while tail_node.next:
+        tail_node = tail_node.next
+    # create circle at index
+    head_node = linked_list_head
+    for i in range(index):
+        head_node = head_node.next
+    tail_node.next = head_node
+
+
+if __name__ == '__main__':
+    nums = [3, 2, 0, -4]
+    circle_index = 1
+    head = list_to_linked_list(nums)
+    # traverse_linked_list(head)
+    create_circle_in_linked_list(head, circle_index)
+    s = Solution()
+    print(s.hasCycle(head))
