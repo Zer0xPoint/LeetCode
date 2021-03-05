b14421868fe0ef62e145e9b163ca45f5c3d5cb15
Zer0xPoint <liyizhou@me.com>
2021-03-05 23:33:31 +0800

    add Solution

--- Linked List 101/3.Classic Problems/Odd Even Linked List.py
+++ Linked List 101/3.Classic Problems/Odd Even Linked List.py
@@ -8,15 +8,27 @@ from ListNode import *
 
 class Solution:
     def oddEvenList(self, head: ListNode) -> ListNode:
-        dummy_head_node = ListNode
-        dummy_head_node.next = head
-        first_node = dummy_head_node
-        second_node = first_node.next
+        if head:
+            odd_index_node = head
+            even_index_node = head.next
+            even_head_node = even_index_node
+            # even_head_node.next = even_index_node
+
+            while even_index_node and even_index_node.next:
+                odd_index_node.next = odd_index_node.next.next
+                even_index_node.next = even_index_node.next.next
+                odd_index_node = odd_index_node.next
+                even_index_node = even_index_node.next
+
+            odd_index_node.next = even_head_node
+
+        return head
 
 
 if __name__ == '__main__':
     nums = [1, 2, 3, 6, 4, 5, 6]
-    # nums = [1]
+    # nums = [1, 2, 3, 4, 5]
+    nums = [1]
     # val = 1
     s = Solution()
     head = list_to_linked_list(nums)
---
+++ Linked List 101/3.Classic Problems/Palindrome Linked List.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from ListNode import *
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+
+
+if __name__ == "__main__":
+    nums = [1, 2, 2, 1]
+    head = list_to_linked_list(nums)
+    s = Solution()
+    print(s.isPalindrome(head))
