9c086970a0fb47b32d4d023f73fc2fc1070629cb
Zer0xPoint <liyizhou@me.com>
2021-03-10 07:44:38 +0800

    add Solution

--- Linked List 101/3.Classic Problems/Palindrome Linked List.py
+++ Linked List 101/3.Classic Problems/Palindrome Linked List.py
@@ -11,18 +11,26 @@ class Solution:
         tail_node = head
         mid_node = head
         while tail_node:
-            tail_node = tail_node.next
-            mid_node = mid_node.next.next
-        prev_node = None
+            if tail_node.next is None:
+                break
+            tail_node = tail_node.next.next
+            mid_node = mid_node.next
+        node = None
         while mid_node:
             next_node = mid_node.next
-            mid_node.next = prev_node
-            prev_node = mid_node
+            mid_node.next = node
+            node = mid_node
             mid_node = next_node
+        while node and head:
+            if node.val is not head.val:
+                return False
+            node = node.next
+            head = head.next
+        return True
 
 
 if __name__ == "__main__":
-    nums = [1, 2, 2, 1]
+    nums = [1, 2, 3, 3, 2, 1]
     head = list_to_linked_list(nums)
     s = Solution()
     print(s.isPalindrome(head))
