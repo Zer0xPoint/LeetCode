159fed135fee5e6519d7f8f3f5194c179d52e6a1
Zer0xPoint <liyizhou@me.com>
2021-03-17 23:59:35 +0800

    add Solution

--- Linked List 101/5.Conclusion/Rotate List.py
+++ Linked List 101/5.Conclusion/Rotate List.py
@@ -13,27 +13,32 @@ class Solution:
         dummy_head = head
         head_node = tail_node = head
         list_len = 1
+        k += 1
         while dummy_head.next:
             list_len += 1
             tail_node = tail_node.next
             dummy_head = dummy_head.next
         tail_node.next = head_node
 
-        for __ in range(k):
-            head_node = head_node.next
-            tail_node = tail_node.next
-            print("head", head_node.val)
-            print("tail", tail_node.val)
+        # for __ in range(k):
+        #     head_node = head_node.next
+        #     tail_node = tail_node.next
+        #     print("head", head_node.val)
+        #     print("tail", tail_node.val)
 
+        for i in range(list_len % k):
+            for j in range(list_len - 1):
+                head_node = head_node.next
+                tail_node = tail_node.next
         tail_node.next = None
 
-        return dummy_head
-        # for __ in range(list_len):
+        return head_node
 
 
 if __name__ == "__main__":
     nums = [1, 2, 3, 4, 5]
-    k = 3
+    nums = [0, 1, 2]
+    k = 2
     head = list_to_linked_list(nums)
     s = Solution()
     traverse_linked_list(s.rotateRight(head, k))
