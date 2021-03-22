12b5c946e006178fb18558751ae46f39708dc4c6
Zer0xPoint <liyizhou@me.com>
2021-03-22 19:58:23 +0800

    add Solution

---
+++ Recursion 101/Recurrence Relation/Reverse Linked List.py
@@ -0,0 +1,10 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from lib.linked_list.linked_list import *
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
