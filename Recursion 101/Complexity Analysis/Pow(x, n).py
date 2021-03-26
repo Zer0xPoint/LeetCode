65351145fbabf4059f25921acb128189c25fb704
Zer0xPoint <liyizhou@me.com>
2021-03-26 17:09:00 +0800

    add Solution

--- Recursion 101/Complexity Analysis/Pow(x, n).py
+++ Recursion 101/Complexity Analysis/Pow(x, n).py
@@ -1,11 +1,22 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-        else:
-            return self.myPow(x, n - 1) * x if n > 0 else 1 / (self.myPow(1 / x, n + 1) * x)
+        # if n == 0:
+        #     return 1
+        # else:
+        #     return self.myPow(x, n - 1) * x if n > 0 else 1 / (self.myPow(1 / x, n + 1) * x)
+
+        res = 1
+        abs_n = abs(n)
+        while abs_n:
+            if abs_n & 1 == 1:
+                res *= x
+            abs_n >>= 1
+            x *= x
+        return (1 / res) if n < 0 else res
 
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.myPow(2, -10))
+    x = 2
+    n = 2
+    print(s.myPow(x, n))
