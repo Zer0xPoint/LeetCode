8f9c75386988f8eb65af590d14df02f04237c672
Zer0xPoint <liyizhou@me.com>
2021-04-01 22:37:15 +0800

    add Solution

--- Binary Tree/2.Solve Problems Recursively/Symmetric Tree.py
+++ Binary Tree/2.Solve Problems Recursively/Symmetric Tree.py
@@ -4,7 +4,19 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        curr_level = [root]
-        while root and curr_level:
+        if not root:
+            return True
+
+        def is_mirror(node_1, node_2):
+            if not node_1 and not node_2:
+                return True
+            if not node_1 or not node_2:
+                return False
+            return node_1.val == node_2.val and is_mirror(node_1.left, node_2.right) and is_mirror(node_1.right,
+                                                                                                   node_2.left)
+
+        return is_mirror(root.left, root.right)
---
+++ Binary Tree/3.Conclusion/Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
