f1cc4c8e231b0b0c3843022e3ec1622272b417a3
Zer0xPoint <liyizhou@me.com>
2021-04-02 20:09:05 +0800

    add Solution

--- Binary Tree/3.Conclusion/Construct Binary Tree from Inorder and Postorder Traversal.py
+++ Binary Tree/3.Conclusion/Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -11,3 +11,14 @@ class TreeNode:
 
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        head = TreeNode(postorder[-1], None, None)
+
+        def sub_tree(node, inorder_list, postorder_list):
+            last_node_val = postorder_list[-1]
+            left_sub_tree_vals = inorder_list[0, inorder_list.index(last_node_val)]
+            right_sub_tree_vals = inorder_list[inorder_list.index(last_node_val), -1]
+
+            if node:
+                if not node.left and not node.right:
+                    return node
+                node.left = sub_tree()
