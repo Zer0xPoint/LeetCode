48df8758e75bfadb612aebb6f843b540c0bd6829
Zer0xPoint <liyizhou@me.com>
2021-04-08 21:45:56 +0800

    add Solution

--- Binary Tree/3.Conclusion/Construct Binary Tree from Preorder and Inorder Traversal.py
+++ Binary Tree/3.Conclusion/Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -1,4 +1,3 @@
-# Definition for a binary tree node.
 from typing import List
 
 
@@ -9,33 +8,32 @@ class TreeNode:
         self.right = right
 
 
-
 class Solution:
-    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
         inorder_index_dict = {}
         for index, value in enumerate(inorder):
             inorder_index_dict[value] = index
 
-        def sub_tree(in_start, in_end, post_start, post_end):
-            if post_start <= post_end:
-                node = TreeNode(postorder[post_end])
+        def sub_tree(in_start, in_end, pre_start, pre_end):
+            if pre_start <= pre_end:
+                node = TreeNode(preorder[pre_start])
                 in_root_index = inorder_index_dict[node.val]
                 in_index_delta = in_root_index - in_start
                 node.left = sub_tree(in_start, in_root_index - 1,
-                                     post_start, post_start + in_index_delta - 1)
+                                     pre_start + 1, pre_start + in_index_delta)
                 node.right = sub_tree(in_start + in_index_delta + 1, in_end,
-                                      post_start + in_index_delta, post_end - 1)
+                                      pre_start + in_index_delta + 1, pre_end)
                 return node
             else:
                 return None
 
-        return sub_tree(0, len(inorder) - 1, 0, len(postorder) - 1)
+        return sub_tree(0, len(preorder) - 1, 0, len(inorder) - 1)
 
 
 if __name__ == "__main__":
     s = Solution()
     inorder_list = [9, 3, 15, 20, 7]
-    postorder_list = [9, 15, 7, 20, 3]
+    preorder_list = [3, 9, 20, 15, 7]
 
-    tree = s.buildTree(inorder_list, postorder_list)
+    tree = s.buildTree(preorder_list, inorder_list)
     print(tree.left.val)
