00e8b1d1f67149599d8e78493635eb9d04ae2be1
Zer0xPoint <liyizhou@me.com>
2021-04-14 20:18:33 +0800

    add Solution

--- Recursion II/1.Divide and Conquer/Quick Sort an Array.py
+++ Recursion II/1.Divide and Conquer/Quick Sort an Array.py
@@ -1,6 +1,24 @@
-class Solution:
+class Solution(object):
     def quick_sort(self, nums, start, end):
-        pass
 
-    def partition(self, pivot_index, start, end):
-        pass
+        def partition(start, end):
+            pivot = nums[end]
+            pivot_index = 0
+            for i in range(start, end):
+                if nums[i] < pivot:
+                    nums[pivot_index], nums[i] = nums[i], nums[pivot_index]
+                    pivot_index += 1
+            nums[end], nums[pivot_index] = nums[pivot_index], nums[end]
+            return pivot_index
+
+        if start < end:
+            pivot_index = partition(start, end)
+            self.quick_sort(nums, start, pivot_index - 1)
+            self.quick_sort(nums, pivot_index + 1, end)
+        # return pivot_index
+
+
+s = Solution()
+nums = [4, 1, 2, 5, 6, 9]
+s.quick_sort(nums, start=0, end=len(nums) - 1)
+print(nums)
