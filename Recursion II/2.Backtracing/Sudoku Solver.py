9af4ea20e8098c3e682acfbcf6133f7de40724e9
Zer0xPoint <liyizhou@me.com>
2021-04-21 21:56:30 +0800

    add Solution

--- Recursion II/2.Backtracing/Sudoku Solver.py
+++ Recursion II/2.Backtracing/Sudoku Solver.py
@@ -6,42 +6,7 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        # def is_sudoku():
-        #     if "." not in set([val for row in board for val in row]):
-        #         return True
-        #
-        # def not_in_row(row, num):
-        #     return False if num in row else True
-        #
-        # def not_in_col(col, num):
-        #     return False if num in col else True
-        #
-        # def not_in_sub_grid(sub_grid, num):
-        #     return False if num in sub_grid else True
-        #
-        # def place_num(row, i, num):
-        #     board[row][i] = num
-        #
-        # def remove_num(row, i):
-        #     board[row][i] = "."
-        #
-        # for row in range(9):
-        #     for i, val in enumerate(board[row]):
-        #         if val == ".":
-        #             col = list(zip(*board))[i]
-        #             sub_row = (row // 3) * 3
-        #             sub_col = (i // 3) * 3
-        #             sub_gird = [board[i][j] for i in range(sub_row, sub_row + 3) for j in
-        #                         range(sub_col, sub_col + 3)]
-        #             for num in range(1, 10):
-        #                 num = str(num)
-        #                 if not_in_col(col, num) and not_in_row(board[row], num) and not_in_sub_grid(sub_gird, num):
-        #                     place_num(row, i, num)
-        #                     if is_sudoku():
-        #                         return
-        #                     else:
-        #                         self.solveSudoku(board)
-        #                     remove_num(row, i)
+
         def sudoku(puzzle):
             # base case: return result if no 0 left
             if "." not in set([v for r in puzzle for v in r]):
@@ -71,7 +36,7 @@ class Solution:
                                 else:
                                     puzzle[row][i] = "."
                         # if iterated over all possible candidates and no continuation - must backtrack
-                        return False
+                        return
 
         def row_ok(entry, row):
             return not (entry in row)
@@ -83,6 +48,7 @@ class Solution:
             return not (entry in square)
 
         sudoku(board)
+
 
 s = Solution()
 board = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
