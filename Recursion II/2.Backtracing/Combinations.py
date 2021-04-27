66e4f45393c17a428948a37c8282368d269e7ecc
Zer0xPoint <liyizhou@me.com>
2021-04-27 14:41:13 +0800

    add Solution

---
+++ Recursion II/2.Backtracing/Combinations.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res_set = set()
+        def helper():
+            pass
+
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    n = 4
+    k = 2
+    print(s.combine(n, k))
