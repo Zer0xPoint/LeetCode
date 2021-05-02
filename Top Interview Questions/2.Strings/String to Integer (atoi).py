7ecf9427a3e895c82af84c286f88ffc81c1ef2a8
Zer0xPoint <liyizhou@me.com>
2021-05-02 23:39:46 +0800

    add Solution

--- Top Interview Questions/2.Strings/String to Integer (atoi).py
+++ Top Interview Questions/2.Strings/String to Integer (atoi).py
@@ -5,15 +5,14 @@ class Solution(object):
     def myAtoi(self, s: str):
         s = s.strip()
         s = re.findall('(^[\+\-0]*\d+)\D*', s)
-
         try:
             result = int(''.join(s))
-            MAX_INT = 2147483647
-            MIN_INT = -2147483648
-            if result > MAX_INT > 0:
-                return MAX_INT
-            elif result < MIN_INT < 0:
-                return MIN_INT
+            max_int = 2147483647
+            min_int = -2147483648
+            if result > max_int > 0:
+                return max_int
+            elif result < min_int < 0:
+                return min_int
             else:
                 return result
         except:
