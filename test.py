aa88e6811c808e79581dc54f44917d0d5801dba5
Yizhou.Li <Zer0xPoint@users.noreply.github.com>
2021-08-19 10:50:05 +0800

    add Solution

--- test.py
+++ test.py
@@ -78,17 +78,21 @@ print(searchInsert([1, 2, 4, 5], 0))
 
 def merge(intervals: List[List[int]]) -> List[List[int]]:
     interval_length = len(intervals)
+    intervals.sort(key=lambda x: x[0])
     if interval_length < 2:
         return intervals
-    delete_flag = []
-    for i in range(interval_length - 1):
-        if intervals[i][-1] >= intervals[i + 1][0]:
-            intervals[i + 1] = [intervals[i][0], intervals[i + 1][-1]]
-            delete_flag.append(i)
-    for i in delete_flag:
-        intervals.remove(intervals[i])
+    merges = list()
+    for interval in intervals:
+        if not merges or merges[-1][-1] < interval[0]:
+            merges.append(interval)
+        else:
+            merges[-1][-1] = max(merges[-1][-1], interval[-1])
+    return merges
 
 
-intervals = [[1,3],[2,6],[8,10],[15,18]]
-merge(intervals)
-print(intervals)
+intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+intervals = [[1, 4], [0, 4]]
+intervals = [[1, 4], [2, 3]]
+print(merge(intervals))
+
+
